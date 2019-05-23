59256e6734ee9b36d903be23f3afb8087e1d7d45
semyon.k <semyon.k@sunrise-r.ru>
2019-05-23 12:27:57 +0300

    Refactor SitesDisplayContext class

--- modules/sunrise-sites-mvc-portlet/src/main/java/sunrise/sites/mvc/portlet/display/SitesDisplayContext.java
+++ modules/sunrise-sites-mvc-portlet/src/main/java/sunrise/sites/mvc/portlet/display/SitesDisplayContext.java
@@ -13,7 +13,6 @@ import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.util.*;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portlet.usersadmin.search.GroupSearch;
-import com.liferay.portlet.usersadmin.search.GroupSearchTerms;
 
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
@@ -122,66 +121,83 @@ public class SitesDisplayContext {
      */
     public GroupSearch getGroupSearchContainer() {
         GroupSearch groupSearch = new GroupSearch(this.renderRequest, this.getPortletURL());
-        GroupSearchTerms searchTerms = (GroupSearchTerms) groupSearch.getSearchTerms();
-        LinkedHashMap<String, Object> groupParams = new LinkedHashMap<>();
-
-        groupParams.put("site", Boolean.TRUE);
-
-        int start = groupSearch.getStart();
-        int end = groupSearch.getEnd();
+        long siteTemplateId = Long.parseLong(sitesTemplateId);
 
         List<Group> groups;
         int groupsCount;
+        int startSearch;
+        int endSearch;
 
-        long layoutId = Long.parseLong(sitesTemplateId);
 
-        if (layoutId != 0) {
-            start = -1;
-            end = -1;
+        if (siteTemplateId == 0) {
+            startSearch = groupSearch.getStart();
+            endSearch = groupSearch.getEnd();
+        } else {
+            startSearch = -1;
+            endSearch = -1;
         }
 
         if (layout.getGroup().isOrganization()) {
-            groupParams.put("active", Boolean.TRUE);
             long organizationId = layout.getGroup().getOrganizationId();
+
             groupsCount = GroupLocalServiceUtil.getOrganizationGroupsCount(organizationId);
-            groups = GroupLocalServiceUtil.getOrganizationGroups(organizationId, start, end);
+
+            groups = GroupLocalServiceUtil.getOrganizationGroups(
+                    organizationId,
+                    startSearch,
+                    endSearch,
+                    groupSearch.getOrderByComparator());
+
         } else {
-            if (layout.getGroup().isUser()) {
-                groupParams.put("usersGroups", layout.getGroup().getClassPK());
-                groupParams.put("active", Boolean.TRUE);
+            LinkedHashMap<String, Object> groupParams = prepareGroupParams();
 
-            } else {
-                List<Integer> types = new ArrayList<>();
-
-                types.add(GroupConstants.TYPE_SITE_OPEN);
-                types.add(GroupConstants.TYPE_SITE_RESTRICTED);
-
-                groupParams.put("types", types);
-                groupParams.put("active", Boolean.TRUE);
-            }
-
-            groupsCount = GroupLocalServiceUtil.searchCount(company.getCompanyId(), searchTerms.getKeywords(), groupParams);
+            groupsCount = GroupLocalServiceUtil.searchCount(company.getCompanyId(), null, groupParams);
 
             groups = GroupLocalServiceUtil.search(
                     company.getCompanyId(),
-                    searchTerms.getKeywords(),
+                    null,
                     groupParams,
-                    start, end,
+                    startSearch, endSearch,
                     groupSearch.getOrderByComparator());
         }
 
-        groupSearch.setTotal(groupsCount);
 
-        if (layoutId != 0) {
-            groups = groups.stream().filter(group -> {
-                try {
-                    return (group.getPublicLayoutSet().getLayoutSetPrototypeId() == layoutId) ||
-                            (group.getPrivateLayoutSet().getLayoutSetPrototypeId() == layoutId);
-                } catch (PortalException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }).collect(Collectors.toList());
+        setGroupSearchResultsAndTotal(groupSearch, siteTemplateId, groups, groupsCount);
+
+        return groupSearch;
+    }
+
+    /**
+     * Prepare group search parameters, based on portlet current layout
+     * @return groupParams
+     */
+    private LinkedHashMap<String, Object> prepareGroupParams() {
+        LinkedHashMap<String, Object> groupParams = new LinkedHashMap<>();
+
+        groupParams.put("active", Boolean.TRUE);
+        groupParams.put("site", Boolean.TRUE);
+
+
+        if (layout.getGroup().isUser()) {
+            groupParams.put("usersGroups", layout.getGroup().getClassPK());
+
+        } else {
+            List<Integer> types = new ArrayList<>();
+
+            types.add(GroupConstants.TYPE_SITE_OPEN);
+            types.add(GroupConstants.TYPE_SITE_RESTRICTED);
+
+            groupParams.put("types", types);
+        }
+        return groupParams;
+    }
+
+    /**
+     * Sets GroupSearch results and total properties, dependant on siteTemplateId
+     */
+    private void setGroupSearchResultsAndTotal(GroupSearch groupSearch, long siteTemplateId, List<Group> groups, int groupsCount) {
+        if (siteTemplateId != 0) {
+            groups = filterGroupsBySiteTemplateId(siteTemplateId, groups);
 
             groupSearch.setTotal(groups.size());
 
@@ -190,16 +206,30 @@ public class SitesDisplayContext {
                             groupSearch.getEnd() < groups.size() ? groupSearch.getEnd() : groups.size()));
 
         } else {
+            groupSearch.setTotal(groupsCount);
             groupSearch.setResults(groups);
         }
+    }
 
-        return groupSearch;
+    /**
+     * @return sites filtered by siteTemplateId
+     */
+    private List<Group> filterGroupsBySiteTemplateId(long siteTemplateId, List<Group> groups) {
+        return groups.stream().filter(group -> {
+            try {
+                return (group.getPublicLayoutSet().getLayoutSetPrototypeId() == siteTemplateId) ||
+                        (group.getPrivateLayoutSet().getLayoutSetPrototypeId() == siteTemplateId);
+            } catch (PortalException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }).collect(Collectors.toList());
     }
 
 
     /**
-     * @param groupId
-     * @return number of members of a group
+     * @param groupId the primary key of the group
+     * @return number of members of the group
      */
     public int getGroupUsersCounts(long groupId) {
         GroupSearch groupSearch = this.getGroupSearchContainer();
